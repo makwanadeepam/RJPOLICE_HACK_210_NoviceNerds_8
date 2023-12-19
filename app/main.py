f9b1a2d99ad1490af08cdbaafd57ae5aa4ab3670
Deepam Makwana <dpammkwana@gmail.com>
2023-12-19 22:37:08 +0530

    Updated main.py

--- app/main.py
+++ app/main.py
@@ -1,18 +1,43 @@
-from flask import render_template, request
-from app import app
+from flask import Flask, render_template, request
+from tensorflow.keras.preprocessing import image
+from tensorflow.keras.applications.inception_v3 import InceptionV3, preprocess_input, decode_predictions
+import numpy as np
 
-# Import your deepfake detection algorithm here (e.g., use a pre-trained model)
+app = Flask(__name__)
+model = InceptionV3(weights='imagenet')
 
-@app.route('/', methods=['GET', 'POST'])
-def index():
+def detect_deepfake(video_path):
+    img = image.load_img(video_path, target_size=(299, 299))
+    img_array = image.img_to_array(img)
+    img_array = np.expand_dims(img_array, axis=0)
+    img_array = preprocess_input(img_array)
+    predictions = model.predict(img_array)
+    decoded_predictions = decode_predictions(predictions)
+    return decoded_predictions
+
+@app.route('/upload', methods=['POST'])
+def upload():
     result = None
-    if request.method == 'POST':
-        pass
-        # Implement deepfake detection algorithm on the uploaded image
-        # Set the result to 'real' or 'fake' based on the detection
 
-        # For example:
-        # result = 'fake'
+    if 'video' not in request.files:
+        return render_template('index.html', result='Error: No video file provided')
+
+    video_file = request.files['video']
+
+    if video_file.filename == '':
+        return render_template('index.html', result='Error: No selected file')
+
+    video_path = 'temp_video.mp4'
+    video_file.save(video_path)
+
+    predictions = detect_deepfake(video_path)
+    result = 'Fake' if predictions else 'Real'
 
     return render_template('index.html', result=result)
 
+@app.route('/')
+def index():
+    return render_template('index.html', result=None)
+
+if __name__ == '__main__':
+    app.run(debug=True)
